cd60c9a55e51b3562e90c85003b642c6538f3d65
itallo2L <itallocerqueiracosta@gmail.com>
2024-09-25 08:04:08 -0300

    Resolvi o terceiro exercício de fixação

---
+++ ExercicioFixacao03/Aluno.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao03
+{
+    class Aluno
+    {
+        public string Nome;
+        public double NotaUm;
+        public double NotaDois;
+        public double NotaTres;
+
+        public double NotaFinal()
+        {
+            double notaFinal = NotaUm + NotaDois + NotaTres;
+            return notaFinal;
+        }
+
+        public void ConsultarSeEstaAprovadoOuNao(double notaFinal)
+        {
+            double media = 60.00;
+            if (notaFinal >= media)
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                double quantidadeDeNotaFaltando = media - notaFinal;
+                Console.WriteLine($"FALTARAM {quantidadeDeNotaFaltando.ToString("F2", CultureInfo.InvariantCulture)} PONTOS");
+            }
+        }
+    }
+}
---
+++ ExercicioFixacao03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+
+namespace ExercicioFixacao03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Escrever("Nome do aluno:");
+            aluno.Nome = Console.ReadLine();
+
+            Escrever("Digite as três notas do aluno:");
+            aluno.NotaUm = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaDois = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaTres = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notaFinal = aluno.NotaFinal();
+
+            Escrever($"NOTA FINAL = {notaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+            aluno.ConsultarSeEstaAprovadoOuNao(notaFinal);
+        }
+
+        public static void Escrever(string texto)
+        {
+            Console.WriteLine(texto);
+        }
+
+        //public static void LerLinhaDouble(double variavel)
+        //{
+        //    variavel = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        //}
+
+        //public static double LerDados(double notaUm, Aluno notaDois, Aluno notaTres)
+        //{
+        //    LerLinhaDouble(notaUm);
+        //    return notaUm;
+        //}
+    }
+}
