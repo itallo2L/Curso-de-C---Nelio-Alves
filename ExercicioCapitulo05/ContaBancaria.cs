fb2993dbfdf80439493875b139edcea6da7f3c0b
itallo2L <itallocerqueiracosta@gmail.com>
2024-10-10 17:19:14 -0300

    Adicionei a Program e a ContaBancaria do Exercício do capítulo 05

---
+++ ExercicioCapitulo05/ContaBancaria.cs
@@ -0,0 +1,34 @@
+﻿namespace ExercicioCapitulo05
+{
+    public class ContaBancaria
+    {
+        public int NumeroDaConta { get; private set; }
+
+        public string NomeDoTitular { get; set; }
+
+        public double ValorDoDepositoInicial { get; set; }
+
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int _numeroDaConta, string _nomeDoTitular, double? _valorDeDeposito) 
+        {
+            NumeroDaConta = _numeroDaConta;
+            NomeDoTitular = _nomeDoTitular;
+
+            if (ValorDoDepositoInicial == 0.0 || ValorDoDepositoInicial == null)
+                Saldo = 0.0;
+        }
+
+        public double Deposito(double valorDoDeposito)
+        {
+            return Saldo += valorDoDeposito;
+        }
+
+        public void Saque(double valorDoSaque)
+        {
+            double taxaDoSaque = 5.0;
+            valorDoSaque += taxaDoSaque;
+            Saldo -= valorDoSaque;
+        }
+    }
+}
---
+++ ExercicioCapitulo05/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace ExercicioCapitulo05
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com o número da conta:");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o titular da conta:");
+            string titularDaConta = Console.ReadLine();
+
+            Console.WriteLine("Haverá depósito inicial (s/n)?");
+            string haDepositoInicial = Console.ReadLine();
+
+            if (haDepositoInicial == "s")
+            {
+                Console.WriteLine("Entre com o valor de depósito inicial:");
+                double valorDeDepositoInicial = double.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
